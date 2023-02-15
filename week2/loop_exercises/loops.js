babaea9235545ad600cd89df5a2509049cbf5e2e
Romaric Zounlome <rzounlom@gmail.com>
2023-02-15 18:56:12 -0500

    updated loops exercise

--- week2/loop_exercises/loops.js
+++ week2/loop_exercises/loops.js
@@ -1,32 +1,25 @@
-/*1. Get the sum of two arrays…actually the sum of all their elements.
-P.S. Each array includes only integer numbers. Output is a number too. 
-let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
-let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
-// Example output: 
-// 276 + 351 = 627*/
-
-/*2. Using a for loop print all even numbers up to and including n. Don’t include 0. 
+/*1. Using a for loop print all even numbers up to and including n. Don’t include 0. 
 let n1 = 22; 
 // Example output: 
 // 2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line*/
 
-/*3. Using a for loop output the elements in reverse order. 
+/*2. Using a for loop output the elements in reverse order. 
 // Example output: 
 // true 3  be  false cannot  true 9 what 43 OR each item on a new line*/
 
-/*4.Given a string change the every second letter to an uppercase ‘Z’. Assume
+/*3.Given a string change the every second letter to an uppercase ‘Z’. Assume
 there are no space.
 // Example output: 
 // jZvZsZrZpZ OR each letter on a new line
 // HINT: You can use  if((i+1) % 2 == 0) to check for even indexes 
 */
 
-/*5. Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
+/*4. Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
 let str2 = "don’t know why";
 // Example output: 
 // “yes”   */
 
-/*6. Given a number n Calculate the factorial of the number
+/*5. Given a number n Calculate the factorial of the number
 // Example output:
 // 24
  */
