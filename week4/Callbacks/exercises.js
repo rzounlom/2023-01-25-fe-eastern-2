106c87ed7cad84b1cd008a7f72956c9f0b3e5e01
Romaric Zounlome <rzounlom@gmail.com>
2023-02-28 21:51:03 -0500

    complete week 4 lesson files

---
+++ week4/Callbacks/exercises.js
@@ -0,0 +1,15 @@
+/**Exercise: Create two callback functions, one for adding two numbers, and one for subtracting two values.
+Using a callback function, return the final calculation of an array of numbers.*/
+//setup arrays
+const myNums = [5, 4, 3, 2, 1];
+const myNums2 = [510, 1, 1, 2, 1];
+
+//step 1: create a function called addNumbers that takes in two parameters, a and b, and returns the sum of a plus b
+
+//step 2: create a function called subtractNumbers that takes in two parameters, a and b, and returns the sum of a minus b
+
+/*create a function called calculateNUms that takes in an array, arr (default arr to an empty array),  and a callback function, func.
+Within the function, use the callback function(func) on each value in the array, and returns the total value.
+HINT: declare a variable called sum and use it to store the calucation of each iteration
+HINT: use a loop
+*/
